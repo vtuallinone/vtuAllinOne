2081c1d86c0a63a2dfa93b480b49dfb8213dd0b4
Ajitpatil92002 <105579551+vtuallinone@users.noreply.github.com>
2022-05-17 21:00:26 +0530

    vtu

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ for(let i=0 ; i < menuLength ; i++){
         menuitem[i].classList.add("active");
     }
 }
+document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
+    var adobeDCView = new AdobeDC.View({clientId: "20ffd9bcf6a344a1a02c37d418b8be4f", divId: "adobe-dc-view"});
+    adobeDCView.previewFile({
+        content:{location: {url: "https://vtuallinone.github.io/vtuAllinOne/pdfs/module2.pdf"}},
+        metaData:{fileName: "Bodea Brochure.pdf"}
+    }, {showAnnotationTools: false, showLeftHandPanel: false, showPageControls: false, 
+        showDownloadPDF: false, showPrintPDF: false});
+});
